35d0ddf1a7f90c33013664f27c8c747158c8c12b
Tania <120820815+svintkovska@users.noreply.github.com>
2023-10-04 18:31:06 +0300

    add sound

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,41 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake instance { get; private set; }
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeTimer;
+
+    private void Awake()
+    {
+        instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+        }
+
+        if (shakeTimer <= 0f)
+        {
+            // Time over!
+            CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+        }
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakeTimer = time;
+    }
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,16 +7,20 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] private float range = 100f;
     [SerializeField] private Camera fpsCam;
+    [SerializeField] private AudioSource gunSound;
 
     private void Awake()
     {
         InputManager inputManager = new InputManager();
         inputManager.Player.Enable();
         inputManager.Player.Shoot.performed += Shoot;
+        gunSound = GetComponent<AudioSource>();
     }
 
     private void Shoot(InputAction.CallbackContext context)
     {
+        gunSound.Play();
+        CameraShake.instance.ShakeCamera(1f, 0.25f);
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
